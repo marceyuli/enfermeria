d3df09ef4c01bba8347f761ad490fc187045e5bc
Yuliana <yulianamarcela200@gmail.com>
2021-06-28 23:58:52 -0400

    Actualizacion seeder de servicio

--- database/seeders/ServicioTableSeeder.php
+++ database/seeders/ServicioTableSeeder.php
@@ -1,6 +1,7 @@
 <?php
 
 namespace Database\Seeders;
+
 use Illuminate\Database\Seeder;
 use Illuminate\Support\Facades\DB;
 
@@ -14,12 +15,99 @@ class ServicioTableSeeder extends Seeder
     public function run()
     {
         DB::table('servicio')->delete();
-      //  DB::table('servicio')->truncate();
-        for($i=1;$i<11;$i++){
+        $servicios = [
+            [
+                'Nombre' => 'Atencion de enfermeria',
+                'Costo' => '30',
+                'Descripcion' => 'Atencion de una enfermera por hora'
+            ],
+            [
+                'Nombre' => 'Cuidado de pacientes',
+                'Costo' => '30',
+                'Descripcion' => 'Cuidado de pacientes por hora'
+            ],
+            [
+                'Nombre' => 'Pacientes en estado critico',
+                'Costo' => '40',
+                'Descripcion' => 'Cuidado de pacientes por hora'
+            ],
+            [
+                'Nombre' => 'Con disfuncion neuromuscular',
+                'Costo' => '40',
+                'Descripcion' => 'Cuidado de pacientes por hora'
+            ],
+            [
+                'Nombre' => 'ACV (Embolia)',
+                'Costo' => '30',
+                'Descripcion' => 'Cuidado de pacientes por hora'
+            ],
+            [
+                'Nombre' => 'Adultos mayores con Alzheimer',
+                'Costo' => '25',
+                'Descripcion' => 'Cuidado de pacientes por hora'
+            ],
+            [
+                'Nombre' => 'Paciente operado',
+                'Costo' => '30',
+                'Descripcion' => 'Cuidado de pacientes por hora'
+            ],
+            [
+                'Nombre' => 'Comfort e higiene a la persona post-tratada',
+                'Costo' => '30',
+                'Descripcion' => 'Cuidado de pacientes por hora'
+            ],
+            [
+                'Nombre' => 'Atencion a recien nacidos, prematuros y pediatricos',
+                'Costo' => '30',
+                'Descripcion' => 'Cuidado de pacientes por hora'
+            ],
+            [
+                'Nombre' => 'Inyectables',
+                'Costo' => '15',
+                'Descripcion' => 'Inyeccion'
+            ],
+            [
+                'Nombre' => 'Suero',
+                'Costo' => '20',
+                'Descripcion' => 'Colocado de suero'
+            ],
+            [
+                'Nombre' => 'Sondas',
+                'Costo' => '20',
+                'Descripcion' => 'Colocado de sonda'
+            ],
+            [
+                'Nombre' => 'Control Glicemia',
+                'Costo' => '10',
+                'Descripcion' => 'Control de glicemia'
+            ],
+            [
+                'Nombre' => 'Curaciones',
+                'Costo' => '30',
+                'Descripcion' => 'Precio por curacion'
+            ],
+            [
+                'Nombre' => 'Nebulizaciones',
+                'Costo' => '30',
+                'Descripcion' => 'Precio por paciente'
+            ],
+            [
+                'Nombre' => 'Preparacion de terapias a domicilio',
+                'Costo' => '100',
+                'Descripcion' => 'Preparacion de terapias a domicilio'
+            ],
+            [
+                'Nombre' => 'Pruebas rapidas COVID 19',
+                'Costo' => '50',
+                'Descripcion' => 'Toma y resultado de prueba rapida COVID-19'
+            ]
+        ];
+
+        foreach ($servicios as $servicio) {
             DB::table('servicio')->insert([
-                'nombre'=>'servicio'.$i,
-                'descripcion'=>'esta es la descripcion'.$i,
-                'costo'=>'costo '.$i
+                'nombre' => $servicio['Nombre'],
+                'costo' => $servicio['Costo'],
+                'descripcion' => $servicio['Descripcion']
             ]);
         }
     }
